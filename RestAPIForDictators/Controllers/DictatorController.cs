43f14883ecab13cdc5b219a5b5f03a3a2f03d8fa
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-26 21:07:24 +0100

    updated

--- RestAPIForDictators/Controllers/DictatorController.cs
+++ RestAPIForDictators/Controllers/DictatorController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestAPIForDictators.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,35 +15,67 @@ namespace RestAPIForDictators.Controllers
 
         // GET: api/<DictatorController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            return JsonSerializer.Serialize(_dictators);
         }
 
         // GET api/<DictatorController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return $"{id}";
+            try
+            {
+                return JsonSerializer.Serialize(_dictators[id]);
+            }
+            catch (Exception)
+            {
+                return "No dictator found";
+            }
         }
 
         // POST api/<DictatorController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string Post([FromBody] DictatorItem dictator)
         {
-            Console.WriteLine($"{value}");
+            DictatorItem _dictator = dictator;
+            _dictator.Id = _dictators.Count;
+            _dictators.Add(_dictator);
+
+            return JsonSerializer.Serialize(_dictator);
         }
 
         // PATCH api/<DictatorController>/5
         [HttpPatch("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public string Patch(int id, [FromBody] DictatorItem dictator)
         {
+            try
+            {
+                _dictators[id] = dictator;
+                _dictators[id].Id = id;
+            }
+            catch (Exception)
+            {
+                return "No dictator found to update";
+            }
+
+            return $"Successfully updated dictator {id}";
         }
 
         // DELETE api/<DictatorController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            try
+            {
+                _dictators.RemoveAt(id);
+            }
+            catch (Exception)
+            {
+                return "No dictator found to remove";
+            }
+
+            return $"Successfully removed dictator {id}";
         }
     }
 }
